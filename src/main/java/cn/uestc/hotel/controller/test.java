9956042958e8d2ee71fee1e8fbacdf3480850672
Aaron2455 <53161606+Aaron2455@users.noreply.github.com>
2019-07-25 08:20:50 +0800

    注释

--- src/main/java/cn/uestc/hotel/controller/test.java
+++ src/main/java/cn/uestc/hotel/controller/test.java
@@ -12,23 +12,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class test {
 
-    @Autowired
+    @Autowired    //Cservice的依赖引入
     private Cservice Cservice;
+//访问服务器跳转页面。
     @RequestMapping("/")
     public String index(Model model) {
 
         return "index.html";
     }
+    //login 的跳转页面
     @RequestMapping("/login")
     public String login(Model model) {
 
         return "login.html";
     }
+    //访问customer的跳转界面
     @RequestMapping("/Customer")
     public String goUserListPage(Model model) {
         model.addAttribute("customer", Cservice.listCustomer());
         return "list.html";
     }
+
+    //访问search的跳转界面。
     @GetMapping("/search")
     public String goUserEditPage(@RequestParam("cid") String id, Model model) {
         Customer customer = Cservice.findUserByPrimaryKey(id);
