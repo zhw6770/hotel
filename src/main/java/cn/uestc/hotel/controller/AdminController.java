d1b09f060231a6f273709f854800e14d18143d5e
Aaron2455 <53161606+Aaron2455@users.noreply.github.com>
2019-07-29 08:45:39 +0800

    增加了hotel的Mapper

---
+++ src/main/java/cn/uestc/hotel/controller/AdminController.java
@@ -0,0 +1,32 @@
+package cn.uestc.hotel.controller;
+
+import cn.uestc.hotel.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+@Controller
+public class AdminController {
+    @Autowired
+    private CustomerService customerService;
+
+
+    @RequestMapping("customerlist")
+    public String getListPage(Model model) {
+
+        model.addAttribute("customer",customerService.findAll());
+        return "customerlist";
+    }
+
+
+    @GetMapping("/delete")
+    @ResponseBody
+    public Boolean deleteUser(@RequestParam("id") String id) {
+        customerService.deleteCustomerByid(id);
+        return true;
+    }
+
+}
---
+++ src/main/java/cn/uestc/hotel/mapper/HotelMapperExt.java
@@ -0,0 +1,4 @@
+package cn.uestc.hotel.mapper;
+
+public interface HotelMapperExt {
+}
