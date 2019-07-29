f1b16738760dfbf87d4023d6ba51f090b6a37bce
Aaron2455 <53161606+Aaron2455@users.noreply.github.com>
2019-07-29 15:09:53 +0800

    搜索展示

--- src/main/java/cn/uestc/hotel/service/CustomerService.java
+++ src/main/java/cn/uestc/hotel/service/CustomerService.java
@@ -47,7 +47,7 @@ public class CustomerService {
         HotelExample.Criteria criteria1 = ex.createCriteria();
         criteria1.andHotelnameLike("%"+word+"%");
         HotelExample.Criteria criteria2 = ex.createCriteria();
-        criteria2.andHotelnameLike("%"+word+"%");
+        criteria2.andAddressLike("%"+word+"%");
         ex.or(criteria2);
 
 
